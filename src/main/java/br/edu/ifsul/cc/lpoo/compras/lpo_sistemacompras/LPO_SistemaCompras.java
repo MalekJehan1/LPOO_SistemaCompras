5fef399e5a0bde7152ad97746c8a9dd360a66bd8
malek <malek@pc-253371.passofundo.ifsul.local>
2024-03-20 11:08:33 -0300

    Criação do projeto

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/compras/lpo_sistemacompras/LPO_SistemaCompras.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.cc.lpoo.compras.lpo_sistemacompras;
+
+/**
+ *
+ * @author malek
+ */
+public class LPO_SistemaCompras {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
